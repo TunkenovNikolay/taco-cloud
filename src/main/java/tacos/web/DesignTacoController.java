fb42f42614717c7951114214658b65165d29d8f8
TunkenovNikolay <nik0sh@bk.ru>
2022-08-20 22:32:26 +0700

    Fix naming ingredient in DesignTacoController

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -27,12 +27,12 @@ public class DesignTacoController {
                 new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
                 new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
                 new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
-                new Ingredient("CARN", "Carnitos", Ingredient.Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
                 new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
                 new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
                 new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
                 new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
-                new Ingredient("SLSA", "Slsa", Ingredient.Type.SAUCE),
+                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
                 new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
         );
 
